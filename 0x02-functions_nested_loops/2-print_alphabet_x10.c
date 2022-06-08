c3f13a442987c88564a3bd49d6f7a832edd7e7ac
Naisianoi <gladyseketo@gmail.com>
2022-06-07 19:56:48 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char alphabet;
+	char letter;
 
 	while (count++ <= 9)
 	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-			_putchar(alphabet);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 
