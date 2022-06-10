0ca238c78a1e9403f52ae2eab79f2eba4171ee5c
Naisianoi <gladyseketo@gmail.com>
2022-06-10 14:22:11 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,8 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf(("Infinite loop incoming :(\n");
-
+	printf("Infinite loop incoming :(\n");
 	i = 0;
 
 	/**
@@ -20,7 +19,6 @@ int main(void)
 	 * }
 	 */
 
-	printf(("Infinite loop avoided! \\o/\n");
-
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
