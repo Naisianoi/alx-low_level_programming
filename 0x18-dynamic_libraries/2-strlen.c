68ff65368c9408a1e9e1974f2e3b82950e7f5f7a
Naisianoi <gladyseketo@gmail.com>
2022-09-09 12:27:22 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: strin6g pointer to be passed to this function.
+ * Return: returns the length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: a parameter to _puts fun6ction6
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
